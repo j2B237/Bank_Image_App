ea8b52d714eb39ef64ff8501eb1f98f417ad286e
yossep237 <yossep237@gmail.com>
2022-02-17 10:19:32 +0000

    add image and video page view

--- Application/controllers/mainController.py
+++ Application/controllers/mainController.py
@@ -13,3 +13,15 @@ def index():
     year = dt.now().strftime("%Y")
     return render_template("main/index.html", title="Flask Bank Image App", categorie="Images",
                            year=year)
+
+
+@main.route("/image-view")
+def image_page():
+    """ Function to render image view """
+    return render_template("main/image_page.html", title="Image page", categorie="Images")
+
+
+@main.route("/video-view")
+def video_page():
+    """ Function to render video view """
+    return render_template("main/video_page.html", title="Video page", categorie="Vidéos")
