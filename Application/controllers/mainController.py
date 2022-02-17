cc8ab154645c6f016e17746fed2f5abf46b4170c
yossep237 <yossep237@gmail.com>
2022-02-17 01:04:37 +0000

    update base.html

--- Application/controllers/mainController.py
+++ Application/controllers/mainController.py
@@ -4,4 +4,4 @@ from flask import render_template
 
 @main.route('/')
 def index():
-    return render_template("main/base.html", title="Flask Bank Image App")
+    return render_template("main/base.html", title="Flask Bank Image App", categorie="Images")
