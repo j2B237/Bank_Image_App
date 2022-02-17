90bf6cddf5fd8fa7b80177e94117334f47079cbe
yossep237 <yossep237@gmail.com>
2022-02-17 01:17:02 +0000

    change links

--- Application/controllers/mainController.py
+++ Application/controllers/mainController.py
@@ -1,7 +1,14 @@
+# Local party import
 from Application.controllers import main
+
+
+# Third party import
 from flask import render_template
+from datetime import datetime as dt
 
 
 @main.route('/')
 def index():
-    return render_template("main/base.html", title="Flask Bank Image App", categorie="Images")
+    year = dt.now().strftime("%Y")
+    return render_template("main/base.html", title="Flask Bank Image App", categorie="Images",
+                           year=year)
